91ecad0a81fa50d7ccb2e72b19e5b1d2d12fade3
Prince <pmdhamecha673@gmail.com>
2023-07-24 12:20:10 +0530

    first commit

---
+++ 01_Getting Started/02_Hello.py
@@ -0,0 +1,9 @@
+print('Hello Mom!!')
+
+# ^ Run:- python 02_Hello.py
+
+# * Interactive Prompt:-
+# ^ python -i 02_Hello.py (quit() or exit() to close)
+
+# * Command as a program:-
+# ^ python -c "print('Hello Mom')"
