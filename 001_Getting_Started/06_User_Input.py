103f4a729a22e9afaebaf76490d31685a6292b61
Prince <pmdhamecha673@gmail.com>
2023-07-26 23:33:20 +0530

    User input added

---
+++ 001_Getting_Started/06_User_Input.py
@@ -0,0 +1,11 @@
+
+# * To get input from the user, use the input function
+
+# * Always input will be of type str
+# name = input("What is your name? ")
+# print("Hello", name)
+
+x = input("Enter Number: ")
+# ! Error
+# print(x/2)
+print(float(x)/2)
